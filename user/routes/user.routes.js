13907fc63d1912fbc388a665afcefaea809a9811
Rahulgit3D2Y <rc2504000@gmail.com>
2025-03-19 16:39:53 +0530

    1

--- user/routes/user.routes.js
+++ user/routes/user.routes.js
@@ -1,11 +1,13 @@
-const express = require("express");
-
+const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/user.controller');
+const authMiddleware = require('../middleware/authMiddleware');
 
-const userController = require("../controllers/user.controller");
-const authMiddleWare = require("../middleware/authMiddleWare");
-router.post("/register", userController.register);
-router.post("/login", userController.login);
-router.post("/logout", userController.logout);
-router.get("/profile", authMiddleWare, userController.profile);
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/logout', userController.logout);
+router.get('/profile', authMiddleware.userAuth, userController.profile);
+router.get('/accepted-ride',authMiddleware.userAuth, userController.acceptedRide);
+
 module.exports = router;
